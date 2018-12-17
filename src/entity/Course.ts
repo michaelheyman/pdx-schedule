eec1f5111c331d921804ddc7c5a0a233c4b2b5e7
michaelheyman <mitch.heyman@gmail.com>
2018-12-16 22:12:56 -0800

    Prettify model files and  set variables to camelCase

--- src/entity/Course.ts
+++ src/entity/Course.ts
@@ -1,56 +1,59 @@
-import {Index,Entity, PrimaryColumn, PrimaryGeneratedColumn, Column, OneToOne, OneToMany, ManyToOne, ManyToMany, JoinColumn, JoinTable, RelationId} from "typeorm";
-import {Instructor} from "./Instructor";
-
+import {
+  Index,
+  Entity,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  OneToMany,
+  ManyToOne,
+  ManyToMany,
+  JoinColumn,
+  JoinTable,
+  RelationId
+} from "typeorm";
+import { Instructor } from "./Instructor";
 
 @Entity("Course")
 export class Course {
+  @Column("integer", {
+    nullable: false,
+    primary: true,
+    name: "Id"
+  })
+  id: number;
 
-    @Column("integer",{ 
-        nullable:false,
-        primary:true,
-        name:"Id"
-        })
-    Id:number;
-        
+  @Column("varchar", {
+    nullable: false,
+    name: "Name"
+  })
+  name: string;
 
-    @Column("varchar",{ 
-        nullable:false,
-        name:"Name"
-        })
-    Name:string;
-        
+  @Column("varchar", {
+    nullable: false,
+    name: "Number"
+  })
+  number: string;
 
-    @Column("varchar",{ 
-        nullable:false,
-        name:"Number"
-        })
-    Number:string;
-        
+  @Column("integer", {
+    nullable: false,
+    name: "CRN"
+  })
+  crn: number;
 
-    @Column("integer",{ 
-        nullable:false,
-        name:"CRN"
-        })
-    CRN:number;
-        
+  @Column("varchar", {
+    nullable: true,
+    name: "URL"
+  })
+  url: string | null;
 
-    @Column("varchar",{ 
-        nullable:true,
-        name:"URL"
-        })
-    URL:string | null;
-        
+  @ManyToOne(type => Instructor, Instructor => Instructor.courses, {})
+  @JoinColumn({ name: "instructor_id" })
+  instructor_: Instructor | null;
 
-   
-    @ManyToOne(type=>Instructor, Instructor=>Instructor.courses,{  })
-    @JoinColumn({ name:'instructor_id'})
-    instructor_:Instructor | null;
-
-
-    @Column("datetime",{ 
-        nullable:true,
-        name:"Timestamp"
-        })
-    Timestamp:Date | null;
-        
+  @Column("datetime", {
+    nullable: true,
+    name: "Timestamp"
+  })
+  timestamp: Date | null;
 }
--- src/entity/Instructor.ts
+++ src/entity/Instructor.ts
@@ -1,62 +1,64 @@
-import {Index,Entity, PrimaryColumn, PrimaryGeneratedColumn, Column, OneToOne, OneToMany, ManyToOne, ManyToMany, JoinColumn, JoinTable, RelationId} from "typeorm";
-import {Course} from "./Course";
-
+import {
+  Index,
+  Entity,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  OneToMany,
+  ManyToOne,
+  ManyToMany,
+  JoinColumn,
+  JoinTable,
+  RelationId
+} from "typeorm";
+import { Course } from "./Course";
 
 @Entity("Instructor")
 export class Instructor {
+  @Column("integer", {
+    nullable: false,
+    primary: true,
+    name: "Id"
+  })
+  id: number;
 
-    @Column("integer",{ 
-        nullable:false,
-        primary:true,
-        name:"Id"
-        })
-    Id:number;
-        
+  @Column("varchar", {
+    nullable: false,
+    name: "FullName"
+  })
+  fullName: string;
 
-    @Column("varchar",{ 
-        nullable:false,
-        name:"FullName"
-        })
-    FullName:string;
-        
+  @Column("varchar", {
+    nullable: true,
+    name: "FirstName"
+  })
+  firstName: string | null;
 
-    @Column("varchar",{ 
-        nullable:true,
-        name:"FirstName"
-        })
-    FirstName:string | null;
-        
+  @Column("varchar", {
+    nullable: true,
+    name: "LastName"
+  })
+  lastName: string | null;
 
-    @Column("varchar",{ 
-        nullable:true,
-        name:"LastName"
-        })
-    LastName:string | null;
-        
+  @Column("float", {
+    nullable: true,
+    name: "Rating"
+  })
+  rating: number | null;
 
-    @Column("float",{ 
-        nullable:true,
-        name:"Rating"
-        })
-    Rating:number | null;
-        
+  @Column("varchar", {
+    nullable: true,
+    name: "URL"
+  })
+  url: string | null;
 
-    @Column("varchar",{ 
-        nullable:true,
-        name:"URL"
-        })
-    URL:string | null;
-        
+  @Column("datetime", {
+    nullable: true,
+    name: "Timestamp"
+  })
+  timestamp: Date | null;
 
-    @Column("datetime",{ 
-        nullable:true,
-        name:"Timestamp"
-        })
-    Timestamp:Date | null;
-        
-
-   
-    @OneToMany(type=>Course, Course=>Course.instructor_)
-    courses:Course[];
-    
+  @OneToMany(type => Course, Course => Course.instructor_)
+  courses: Course[];
 }
