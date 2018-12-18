657ceeda64577e329504fd3313fc970e94064060
michaelheyman <mitch.heyman@gmail.com>
2018-12-17 19:45:21 -0800

    Add course decoder

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,7 +1,8 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, pre, text)
+import Html exposing (Html, div, pre, table, tbody, td, text, th, thead, tr)
+import Html.Attributes exposing (class)
 import Http
 import Json.Decode as Decode
 
@@ -28,6 +29,17 @@ type Model
     | Success String
 
 
+type alias Course =
+    { id : Int
+    , name : String
+    , number : String
+    , crn : Int
+    , url : Maybe String
+    , instructor_id : Maybe Int
+    , timestamp : String
+    }
+
+
 type alias Instructor =
     { id : Int
     , fullName : String
@@ -42,7 +54,7 @@ type alias Instructor =
 init : () -> ( Model, Cmd Msg )
 init _ =
     ( Loading
-    , requestInstructor
+    , getCourse
     )
 
 
@@ -60,6 +72,18 @@ getInstructor =
         { url = "http://localhost:3000/instructor/1"
         , expect = Http.expectJson GotInstructor instructorDecoder
         }
+
+
+getCourse : Cmd Msg
+getCourse =
+    Http.get
+        { url = "http://localhost:3000/course/1"
+        , expect = Http.expectJson GotCourse courseDecoder
+        }
+
+
+
+{- This is just a more complicated version of getInstructor, they work the same -}
 
 
 requestInstructor : Cmd Msg
@@ -82,6 +106,7 @@ requestInstructor =
 type Msg
     = GotTest (Result Http.Error String)
     | GotInstructor (Result Http.Error Instructor)
+    | GotCourse (Result Http.Error Course)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -103,6 +128,14 @@ update msg model =
                 Err error ->
                     ( Failure error, Cmd.none )
 
+        GotCourse result ->
+            case result of
+                Ok value ->
+                    ( Success "course found!", Cmd.none )
+
+                Err error ->
+                    ( Failure error, Cmd.none )
+
 
 instructorDecoder : Decode.Decoder Instructor
 instructorDecoder =
@@ -114,6 +147,19 @@ instructorDecoder =
         (Decode.maybe (Decode.field "lastName" Decode.string))
         (Decode.maybe (Decode.field "rating" Decode.float))
         (Decode.maybe (Decode.field "url" Decode.string))
+        (Decode.field "timestamp" Decode.string)
+
+
+courseDecoder : Decode.Decoder Course
+courseDecoder =
+    Decode.map7
+        Course
+        (Decode.field "id" Decode.int)
+        (Decode.field "name" Decode.string)
+        (Decode.field "number" Decode.string)
+        (Decode.field "crn" Decode.int)
+        (Decode.maybe (Decode.field "url" Decode.string))
+        (Decode.maybe (Decode.field "instructor_id" Decode.int))
         (Decode.field "timestamp" Decode.string)
 
 
@@ -137,6 +183,11 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
+    viewTest model
+
+
+viewTest : Model -> Html Msg
+viewTest model =
     case model of
         Failure error ->
             text (Debug.toString error)
@@ -146,3 +197,31 @@ view model =
 
         Success fullText ->
             pre [] [ text fullText ]
+
+
+viewTable : Html Msg
+viewTable =
+    div [ class "col-md-4" ]
+        [ table [ class "table table-striped" ]
+            [ thead []
+                [ tr []
+                    [ th [] [ text "Column 1" ]
+                    , th [] [ text "Column 2" ]
+                    ]
+                ]
+            , tbody []
+                [ tr []
+                    [ td [] [ text "Value 1" ]
+                    , td [] [ text "Value 2" ]
+                    ]
+                , tr []
+                    [ td [] [ text "Value 3" ]
+                    , td [] [ text "Value 4" ]
+                    ]
+                , tr []
+                    [ td [] [ text "Value 5" ]
+                    , td [] [ text "Value 6" ]
+                    ]
+                ]
+            ]
+        ]
