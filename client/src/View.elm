696085e71ebdf361f642dbad549cc8d6a4b9e627
michaelheyman <mitch.heyman@gmail.com>
2018-12-31 17:01:20 -0800

    Make page more responsive and add some design

--- client/src/View.elm
+++ client/src/View.elm
@@ -4,61 +4,83 @@ import Bootstrap.Alert as Alert
 import Bootstrap.CDN as CDN
 import Bootstrap.Form.Input as Input
 import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Grid.Row as Row
 import Bootstrap.Progress as Progress
 import Bootstrap.Table as Table
-import Html exposing (Html, a, br, div, footer, text)
-import Html.Attributes exposing (align, class, colspan, href, style)
+import Bootstrap.Text as Text
+import Bootstrap.Utilities.Flex as Flex
+import Html exposing (Html, a, br, div, footer, h1, i, li, main_, p, span, text, ul)
+import Html.Attributes exposing (align, attribute, autocomplete, class, colspan, href, style, target)
 import Html.Lazy exposing (lazy)
 import Http
 import Model exposing (..)
 import Round exposing (round)
+import Styles exposing (..)
 
 
 view : Model -> Html Msg
 view model =
-    Grid.container []
-        [ CDN.stylesheet
-        , case model.response of
+    div []
+        (case model.response of
             Loading ->
-                viewProgressBar model.loadingValue
+                [ viewProgressBar model.loadingValue ]
 
             Success ->
-                div []
-                    [ viewInput
-                    , br [] []
-                    , viewTable model
-                    , br [] []
-                    , div [ align "right" ]
-                        [ text "Last Updated: "
-                        , viewTimestamp model
-                        ]
-                    ]
+                renderPage model
 
             Failure _ ->
-                Alert.simpleDanger []
+                [ Alert.simpleDanger []
                     [ Html.strong [] [ text "Oh snap! " ]
                     , text "There was a problem loading the page."
                     ]
+                ]
+        )
+
+
+renderPage : Model -> List (Html Msg)
+renderPage model =
+    [ pageHeader
+    , Grid.containerFluid []
+        [ Grid.row
+            [ Row.centerSm ]
+            [ Grid.col
+                [ Col.xs12, Col.md12, Col.xl10, Col.pullMd2, Col.attrs [ class "bd-content" ] ]
+                [ main_
+                    []
+                    (viewPage model)
+                ]
+            ]
+        ]
+    , viewFooter model
+    ]
+
+
+pageHeader : Html Msg
+pageHeader =
+    div
+        pageHeaderStyle
+        [ div
+            [ class "container" ]
+            [ h1 [] [ text "PSU Class Schedule" ] ]
         ]
 
 
-viewTimestamp : Model -> Html Msg
-viewTimestamp model =
-    case List.head <| List.sortBy .timestamp model.courses of
-        Just course ->
-            text course.timestamp
-
-        Nothing ->
-            text ""
+viewPage : Model -> List (Html Msg)
+viewPage model =
+    [ viewInput
+    , br [] []
+    , lazy courseTable model
+    ]
 
 
 viewInput : Html Msg
 viewInput =
     Input.search
         [ Input.id "searchInput"
-        , Input.small
         , Input.placeholder "Search for class.."
         , Input.onInput Search
+        , Input.attrs inputBoxStyle
         ]
 
 
@@ -71,28 +93,21 @@ viewProgressBar value =
         ]
 
 
-viewTable : Model -> Html Msg
-viewTable model =
-    Grid.row []
-        [ Grid.col []
-            [ lazy courseTable model ]
-        ]
-
-
 courseTable : Model -> Html Msg
 courseTable model =
     Table.table
-        { options = [ Table.hover, Table.responsive, Table.striped, Table.small ]
+        { options = [ Table.hover, Table.responsiveXl, Table.striped ]
         , thead =
             Table.thead [ Table.headAttr (class "thead-dark") ]
                 [ Table.tr []
                     [ Table.th hiddenCell [ text "Id" ]
                     , Table.th [] [ text "Class" ]
                     , Table.th [] [ text "Name" ]
-                    , Table.th [] [ text "Days" ]
-                    , Table.th [] [ text "Time" ]
-                    , Table.th [] [ text "Credits" ]
-                    , Table.th hiddenCell [ text "CRN" ]
+                    , Table.th hiddenSm [ text "Days" ]
+                    , Table.th hiddenSm [ text "Time" ]
+                    , Table.th hiddenMd [ text "Credits" ]
+
+                    --, Table.th hiddenCell [ text "CRN" ]
                     , Table.th [] [ text "Instructor" ]
                     , Table.th [] [ text "Rating" ]
                     ]
@@ -114,22 +129,25 @@ courseTable model =
 
 courseRow : Course -> Table.Row msg
 courseRow course =
-    Table.tr []
+    Table.tr [ Table.rowAttr Flex.col ]
         [ Table.td hiddenCell [ text (String.fromInt course.id) ]
         , Table.td [] [ text course.number ]
         , Table.td [] [ text course.name ]
-        , Table.td []
+        , Table.td
+            hiddenSm
             [ Maybe.map (\days -> text days) course.days
                 |> Maybe.withDefault (text "")
             ]
-        , Table.td []
+        , Table.td
+            hiddenSm
             [ Maybe.map (\time -> text time) course.time
                 |> Maybe.withDefault (text "")
             ]
-        , Table.td [] [ text (String.fromInt course.credits) ]
-        , Table.td hiddenCell [ text (String.fromInt course.crn) ]
         , Table.td
-            []
+            hiddenMd
+            [ text (String.fromInt course.credits) ]
+        , Table.td hiddenCell [ text (String.fromInt course.crn) ]
+        , Table.td []
             [ Maybe.map viewName course.instructor
                 |> Maybe.withDefault (text "")
             ]
@@ -148,12 +166,60 @@ viewName instructor =
 
 viewRating : Instructor -> Html msg
 viewRating instructor =
-    Maybe.map2 (\r u -> a [ href u ] [ text <| round 1 r ]) instructor.rating instructor.url
+    Maybe.map2 (\r u -> externalLink u (round 1 r)) instructor.rating instructor.url
         |> Maybe.withDefault (text "")
 
 
-hiddenCell : List (Table.CellOption msg)
-hiddenCell =
-    [ Table.cellAttr (style "display" "none")
-    , Table.cellAttr (style "visibility" "hidden")
-    ]
+viewTimestamp : Model -> Html Msg
+viewTimestamp model =
+    case List.head <| List.sortBy .timestamp model.courses of
+        Just course ->
+            text
+                (String.dropRight 5
+                    (String.map
+                        (\x ->
+                            if Char.isAlpha x then
+                                ' '
+                            else
+                                x
+                        )
+                        course.timestamp
+                    )
+                )
+
+        Nothing ->
+            text ""
+
+
+viewFooter : Model -> Html Msg
+viewFooter model =
+    footer
+        footerStyle
+        [ div [ class "container" ]
+            [ div
+                [ Flex.block
+                , Flex.justifyBetween
+                ]
+                [ span
+                    [ class "bd-footer-links" ]
+                    [ i [ class "fa fa-github", attribute "aria-hidden" "true" ] []
+                    , externalLink "https://github.com/michaelheyman/pdx-schedule/" " Source"
+                    ]
+                , span
+                    []
+                    [ text "Last Updated: "
+                    , viewTimestamp model
+                    ]
+                ]
+            , br [] []
+            , div []
+                [ p [] [ text "The contents of this page are not sanctioned by Portland State University." ]
+                ]
+            ]
+        ]
+
+
+externalLink : String -> String -> Html msg
+externalLink url label =
+    a [ href url, target "_blank" ]
+        [ text label ]
