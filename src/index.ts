bf807743541a42c5c7f98d41663861aed740fc86
michaelheyman <mitch.heyman@gmail.com>
2019-01-21 21:19:59 -0800

    Add HTTPS capability

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { Request, Response } from "express";
 import * as express from "express";
+import * as fs from "fs";
+import * as https from "https";
+import * as http from "http";
 import * as bodyParser from "body-parser";
 import { AppRoutes } from "./routes";
 
@@ -42,5 +45,37 @@ createConnection()
                 `Express application is up and running on http://localhost:${PORT}`
             );
         });
+
+        if (process.env.NODE_ENV) {
+            // allow https connections
+            const privateKey = fs.readFileSync(
+                "/etc/letsencrypt/live/${process.env.HOST_NAME}/privkey.pem",
+                "utf8"
+            );
+            const certificate = fs.readFileSync(
+                "/etc/letsencrypt/live/${process.env.HOST_NAME}/cert.pem",
+                "utf8"
+            );
+            const ca = fs.readFileSync(
+                "/etc/letsencrypt/live/${process.env.HOST_NAME}/chain.pem",
+                "utf8"
+            );
+
+            const credentials = {
+                key: privateKey,
+                cert: certificate,
+                ca: ca
+            };
+
+            // start both http and https servers
+            const httpServer = http.createServer(app);
+            const httpsServer = https.createServer(credentials, app);
+            httpServer.listen(80, () => {
+                console.log("HTTP Server running on port 80");
+            });
+            httpsServer.listen(443, () => {
+                console.log("HTTPS Server running on port 443");
+            });
+        }
     })
     .catch(error => console.log("TypeORM connection error: ", error));
